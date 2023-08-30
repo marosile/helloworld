e928dbeb1ad0b3d571bb06fc2d69dbec5bc2a7b4
abs013r <abs013r@gmail.com>
2023-08-30 11:00:43 +0900

    no message

---
+++ src/main/webapp/resources/js/common/header.js
@@ -0,0 +1,12 @@
+$('#category > a').hover(function() {
+    switch ($(this).text()) {
+        case '커뮤니티':
+            break;
+        case '채용':
+            break;
+        case '강의':
+            break;
+        case '스터디':
+            break;
+    }
+});
