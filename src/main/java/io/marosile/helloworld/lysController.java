04ec130b920780e5221beee1794372bc07ec7282
lys <lys8845@gmail.com>
2023-09-01 18:59:52 +0900

    2023-09-21 휴대폰 인증 성공~

--- src/main/java/io/marosile/helloworld/lysController.java
+++ src/main/java/io/marosile/helloworld/lysController.java
@@ -1,14 +1,80 @@
 package io.marosile.helloworld;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.io.IOException;
+import java.util.Map;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import io.marosile.helloworld.common.utility.Util;
+
+
+@SessionAttributes("rand")
 @Controller
 public class lysController {
 	
+	// 회원가입
 	@GetMapping("/signUp")
 	public String signUp() {
 		return "common/modal/signUp";
 	}
+	
+	@GetMapping("/findId")
+	public String findId() {
+		return "common/findId";
+	}
+	
+	
+	// 휴대폰 인증
+	@PostMapping("phoneAuth")
+	@ResponseBody
+	public String phoneAuth(@RequestBody Map<String, Object> inputTel1
+						, Model model) {
+		
+		String rand = null;
+		try {
+			String tel = inputTel1.get("inputTel1").toString();
+			
+			rand = Util.sendRandomMessage(tel);
+			
+			model.addAttribute("rand", rand);
+			
+			Util util = new Util();
+			util.sendMessage(tel, rand);
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return rand;
+		
+	}
+	
+	
+	
+	@PostMapping("phoneAuthCheck")
+	@ResponseBody
+	public int phoneAuthCheck(@RequestBody Map<String, Object> inputTel2,
+					@SessionAttribute("rand") String rand) {
+		
+		String randCheck = inputTel2.get("inputTel2").toString();
+		
+		System.out.println(randCheck);
+		
+		if(rand.equals(randCheck)) {
+			return 1;
+		}else {
+			return 0;
+		}
+		
+	}
+	
 
 }
