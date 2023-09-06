8f8d707e447969c3f1db0f135641bc94bf0da51e
OHS <gudtjr1355@gmail.com>
2023-09-06 19:39:33 +0900

    ㅋㅁ

--- src/main/webapp/resources/js/board/board-detail.js
+++ src/main/webapp/resources/js/board/board-detail.js
@@ -13,3 +13,20 @@ const list = document.getElementById("list");
 list.addEventListener("click", function(){
 
 })
+
+
+/* 게시글 신고 모달창 */
+const report = document.getElementById("report");
+const reportzone = document.getElementById("report-area");
+const close = document.getElementById("close");
+
+
+report.addEventListener("click", () => {
+
+    reportzone.classList.toggle("report-area");
+})
+
+close.addEventListener("click", () => {
+
+    reportzone.classList.toggle("report-area");
+})
