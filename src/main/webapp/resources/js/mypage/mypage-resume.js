21e82493d8ff546ac487256c00843a8caa5f122a
cje <cje960215@naver.com>
2023-09-06 09:03:13 +0900

    9:03분 첫 커밋쓰

---
+++ src/main/webapp/resources/js/mypage/mypage-resume.js
@@ -0,0 +1,18 @@
+/* 팝업 영역 */
+const seeBtn1 = document.getElementById("#seeBtn1");
+
+const see1popup = document.getElementById("#see1popup");
+
+const closeBtn = document.getElementById("#closeBtn");
+
+seeBtn1.addEventListener("click", function(){
+
+    seeBtn1.style.color="red";
+
+})
+
+
+
+
+
+ 
