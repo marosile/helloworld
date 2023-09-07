ac7b396226dcfe97d00101134308764146ac7468
cje <cje960215@naver.com>
2023-09-07 12:01:44 +0900

    오늘 첫커밋

--- src/main/webapp/resources/js/mypage/mypage-resume.js
+++ src/main/webapp/resources/js/mypage/mypage-resume.js
@@ -1,15 +1,38 @@
 /* 팝업 영역 */
-const seeBtn1 = document.getElementById("#seeBtn1");
+/* row2 요소들 */
+const popupBtn1 = document.getElementById("popupBtn1"); // 보기 버튼
+const popup1 = document.getElementById("popup1"); // 팝업창
+const closeBtn1 = document.getElementById("closeBtn1"); // 닫기 버튼
 
-const see1popup = document.getElementById("#see1popup");
 
-const closeBtn = document.getElementById("#closeBtn");
+/* row3 요소 */
+const popupBtn2 = document.getElementById("popupBtn2"); // 보기버튼
+const popup2 = document.getElementById("popup2"); // 팝업창
+const closeBtn2 = document.getElementById("closeBtn2"); // 닫기 버튼
 
-seeBtn1.addEventListener("click", function(){
 
-    seeBtn1.style.color="red";
+closeBtn1.addEventListener("click", e =>{
+    popup1.classList.toggle("popup");
 
-})
+});
+popupBtn1.addEventListener("click", e => {
+    popup1.classList.toggle("popup");
+
+});
+
+
+closeBtn2.addEventListener("click", e =>{
+    popup2.classList.toggle("popup");
+});
+popupBtn2.addEventListener("click", e => {
+    popup2.classList.toggle("popup");
+});
+
+
+
+    
+
+
 
 
 
