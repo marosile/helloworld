fdbfdff6031657dcffa84de98b1e882503426267
lys <lys8845@gmail.com>
2023-09-12 14:32:35 +0900

    Changes

--- src/main/java/io/marosile/helloworld/member/model/service/MemberService.java
+++ src/main/java/io/marosile/helloworld/member/model/service/MemberService.java
@@ -1,5 +1,20 @@
 package io.marosile.helloworld.member.model.service;
 
+
+import java.util.HashMap;
+
 public interface MemberService {
+
+    /** 카카오 인증 access_Token 얻기
+     * @param code
+     * @return
+     */
+    String getAccessToken(String code) throws Throwable;
+
+    /** 유저 정보 얻어오기
+     * @param access_Token
+     * @return
+     */
+    HashMap<String, Object> getUserInfo(String access_Token);
 
 }
