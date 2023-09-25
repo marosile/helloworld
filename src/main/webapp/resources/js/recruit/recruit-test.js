7438e4c8e439d0c1a88123d262fe946965f353d5
PHJ <ssr3194@gmail.com>
2023-09-25 09:16:32 +0900

    커밋~

--- src/main/webapp/resources/js/recruit/recruit-test.js
+++ src/main/webapp/resources/js/recruit/recruit-test.js
@@ -229,3 +229,27 @@ document.getElementById("button7").addEventListener("click", () => {
     document.body.appendChild(form);
     form.submit();
 });
+
+
+var answer = document.getElementsByClassName("answer");
+
+function handleClick(event) {
+
+  if (event.target.classList[1] === "clicked") {
+    event.target.classList.remove("clicked");
+  } else {
+    for (var i = 0; i < answer.length; i++) {
+        answer[i].classList.remove("clicked");
+    }
+
+    event.target.classList.add("clicked");
+  }
+}
+
+function init() {
+  for (var i = 0; i < answer.length; i++) {
+    answer[i].addEventListener("click", handleClick);
+  }
+}
+
+init();
