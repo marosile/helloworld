f7234a7bdcc35a728356fc16d1de13b708d8f084
OHS <gudtjr1355@gmail.com>
2023-09-26 18:05:29 +0900

    no message

--- src/main/webapp/resources/js/recruit/recruit-moreDetail.js
+++ src/main/webapp/resources/js/recruit/recruit-moreDetail.js
@@ -1,13 +1,3 @@
-
-// 이력서 작성 페이지로
-const applyBtn = document.getElementById("applyBtn");
-
-applyBtn.addEventListener("click", () => {
-    
-    location.href = "apply";
-
-})
-
 /* 북마크 */
 const bookMark = document.getElementById("bookMark");
 
